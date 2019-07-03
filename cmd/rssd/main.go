3d758dd8ebb8b6603ab077cf2ea860d16a49aef1
Marcel Voigt <mv@noch.so>
2019-07-03 22:00:16 +0200

    Use structured logger

--- cmd/rssd/main.go
+++ cmd/rssd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
+
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/logfmt"
 )
 
 var (
@@ -16,15 +18,16 @@ var (
 )
 
 func main() {
+	log.SetHandler(logfmt.Default)
 	flag.StringVar(&httpAddr, "http", httpAddr, "HTTP listening address")
 	flag.DurationVar(&httpGrace, "grace", httpGrace, "HTTP shutdown grace period for existing connections")
 	flag.Parse()
 
 	err := run()
 	if err != nil {
-		log.Fatal(err)
+		log.WithError(err).Fatal("fatal error")
 	}
-	log.Print("exit")
+	log.Info("exit")
 }
 
 func run() error {
@@ -38,17 +41,17 @@ func run() error {
 		sigint := make(chan os.Signal, 1)
 		signal.Notify(sigint, os.Interrupt)
 		<-sigint
-		log.Printf("interrupt signal received. shutting down HTTP server with grace period of %v", httpGrace)
+		log.WithField("timeout", httpGrace).Info("interrupt signal received. shutting down HTTP server with timeout for existing connections.")
 		ctx, cancel := context.WithTimeout(context.Background(), httpGrace)
 		defer cancel()
 		if err := srv.Shutdown(ctx); err != nil {
 			// Error from closing listeners, or context timeout:
-			log.Printf("HTTP server Shutdown: %v", err)
+			log.WithError(err).Error("HTTP server Shutdown")
 		}
 		close(idleConnsClosed)
 	}()
 
-	log.Printf("HTTP server starting to listen on %v", httpAddr)
+	log.WithField("http_addr", httpAddr).Info("HTTP server starting to listen")
 	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		// Error starting or closing listener
 		return err
