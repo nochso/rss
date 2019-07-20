0363e1d0830c56cbf802b7ec43d6693e39a678a2
Marcel Voigt <mv@noch.so>
2019-07-20 15:24:04 +0200

    Indent using spaces in SQL statements

--- cmd/rssd/db.go
+++ cmd/rssd/db.go
@@ -68,47 +68,47 @@ CREATE TABLE user_feed_item_read (
 );
 
 CREATE TABLE subscription (
-	id              PRIMARY KEY
-					NOT NULL,
-	-- Delete subscriptions when deleting user
-	user_id INTEGER REFERENCES user (id) ON DELETE CASCADE
-					NOT NULL,
-	-- Forbid deleting feeds that have a subscription
-	feed_id INTEGER REFERENCES feed (id) ON DELETE RESTRICT
-					NOT NULL,
-	UNIQUE (
-		user_id,
-		feed_id
-	)
+    id              PRIMARY KEY
+                    NOT NULL,
+    -- Delete subscriptions when deleting user
+    user_id INTEGER REFERENCES user (id) ON DELETE CASCADE
+                    NOT NULL,
+    -- Forbid deleting feeds that have a subscription
+    feed_id INTEGER REFERENCES feed (id) ON DELETE RESTRICT
+                    NOT NULL,
+    UNIQUE (
+        user_id,
+        feed_id
+    )
 );
 
 CREATE TABLE subscription_tag (
-	id              INTEGER PRIMARY KEY
-							NOT NULL,
-	-- Delete subscription tags when deleting subscription
-	subscription_id INTEGER REFERENCES subscription (id) ON DELETE CASCADE
-							NOT NULL,
-	-- Forbid deleting tags used by a subscription
-	tag_id          INTEGER REFERENCES tag (id) ON DELETE RESTRICT
-							NOT NULL,
-	UNIQUE (
-		subscription_id,
-		tag_id
-	)
+    id              INTEGER PRIMARY KEY
+                            NOT NULL,
+    -- Delete subscription tags when deleting subscription
+    subscription_id INTEGER REFERENCES subscription (id) ON DELETE CASCADE
+                            NOT NULL,
+    -- Forbid deleting tags used by a subscription
+    tag_id          INTEGER REFERENCES tag (id) ON DELETE RESTRICT
+                            NOT NULL,
+    UNIQUE (
+        subscription_id,
+        tag_id
+    )
 );
 
 CREATE TABLE tag (
-	id   INTEGER PRIMARY KEY
-					NOT NULL,
-	name VARCHAR NOT NULL
-					UNIQUE
+    id   INTEGER PRIMARY KEY
+                 NOT NULL,
+    name VARCHAR NOT NULL
+                 UNIQUE
 );
 
 CREATE TABLE user (
-	id INTEGER NOT NULL
-				PRIMARY KEY,
-	email VARCHAR NOT NULL
-					UNIQUE
+    id    INTEGER NOT NULL
+                  PRIMARY KEY,
+    email VARCHAR NOT NULL
+                  UNIQUE
 );`),
 }
 
