fa74c127544b403ec3801b89898dcdd801017896
Marcel Voigt <mv@noch.so>
2019-07-20 17:00:30 +0200

    Extract waitForShutdown

--- cmd/rssd/main.go
+++ cmd/rssd/main.go
@@ -52,19 +52,7 @@ func run() error {
 	}
 
 	idleConnsClosed := make(chan struct{})
-	go func() {
-		sigint := make(chan os.Signal, 1)
-		signal.Notify(sigint, os.Interrupt)
-		<-sigint
-		log.WithField("timeout", httpGrace).Info("interrupt signal received. shutting down HTTP server with timeout for existing connections.")
-		ctx, cancel := context.WithTimeout(context.Background(), httpGrace)
-		defer cancel()
-		if err := srv.Shutdown(ctx); err != nil {
-			// Error from closing listeners, or context timeout:
-			log.WithError(err).Error("HTTP server Shutdown")
-		}
-		close(idleConnsClosed)
-	}()
+	go waitForShutdown(srv, idleConnsClosed)
 
 	log.WithField("http_addr", httpAddr).Info("HTTP server starting to listen")
 	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
@@ -73,4 +61,18 @@ func run() error {
 	}
 	<-idleConnsClosed
 	return nil
+}
+
+func waitForShutdown(srv *http.Server, idleConnsClosed chan struct{}) {
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+	<-sigint
+	log.WithField("timeout", httpGrace).Info("interrupt signal received. shutting down HTTP server with timeout for existing connections.")
+	ctx, cancel := context.WithTimeout(context.Background(), httpGrace)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		// Error from closing listeners, or context timeout:
+		log.WithError(err).Error("HTTP server Shutdown")
+	}
+	close(idleConnsClosed)
 }
